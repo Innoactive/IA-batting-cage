64996cff73914b2434cfbc861f481edf44b80cf7
Harprasad <harprasad@ensoimmersive.com>
2018-08-06 20:04:23 +0530

    Added NearMiss

--- Assets/Scripts/BallEventsObservable.cs
+++ Assets/Scripts/BallEventsObservable.cs
@@ -6,12 +6,15 @@ public class BallEventsObservable : MonoBehaviour {
 
     public delegate void Ballhit(GameObject with,GameObject ball);
     public delegate void BallShot();
+    public delegate void NearMiss();
 
+    public static event NearMiss OnNearMiss;
     public static event BallShot OnBallShot;
     public static event Ballhit OnBallHit;
 
+    bool hasTouchedBat = false;
     public void ReportBallShoot() {
-
+        hasTouchedBat = false;
         if (OnBallShot != null)
         {
             OnBallShot();
@@ -23,10 +26,20 @@ public class BallEventsObservable : MonoBehaviour {
         if (OnBallHit != null) {
             OnBallHit(collision.gameObject,gameObject);
         }
+
+        if(!hasTouchedBat && collision.gameObject.tag.Equals("Bat"))
+        {
+            hasTouchedBat = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         //TODO  implement Nearmiss
+        if (!hasTouchedBat && other.tag.Equals("Bat")) {
+            if (OnNearMiss != null) {
+                OnNearMiss();
+            }
+        }
     }
 }
